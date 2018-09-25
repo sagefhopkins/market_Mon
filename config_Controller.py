8480afad0ec3d32c70a03b5cb5ffe00edc123e5a
sagefhopkins <sagefhopkins@gmail.com>
2018-09-25 22:24:36 +0900

    Removing non critical print outs

--- config_Controller.py
+++ config_Controller.py
@@ -14,15 +14,12 @@ def config_Add_Cur(currency):
     print currency + " added to config.ini"
 
 def config_Load_Cur():
-    print "config_Load_Cur"
     with open('config.ini') as f:
         conf = f.read()
     config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.readfp(io.BytesIO(conf))
     for each_section in config.sections():
         for (each_key, each_val) in config.items(each_section):
-            print each_key
-            print each_val
             whileloop = 1
             if each_val == 'True':
                 while whileloop == 1:
@@ -31,12 +28,10 @@ def config_Load_Cur():
                         final = jsc.regex_Seperate(data)
                         dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
                         print final[0] + final[1] + final[2] + final[3] + " inserted into database"
-                        print "Wating a few seconds"
-                        time.sleep(5)
-                        print "Done sleeping!"
+                        time.sleep(1)
                         whileloop = 0
                     except (IOError, TypeError, KeyError):
-                        print "Error encountered moving on"
-                        time.sleep(15)
+                        print "WebAPI Timed out, Retrying"
+                        time.sleep(3)
             else:
                 print "config_Load_Cur failed"
