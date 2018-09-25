582b6a9623c8f44373aeff46fba9d4f562128151
sagefhopkins <sagefhopkins@gmail.com>
2018-09-25 21:37:25 +0900

    More Testing

--- json_Controller.py
+++ json_Controller.py
@@ -14,56 +14,59 @@ def json_Import_Cur(currency):
 
     return data
 def regex_Seperate(data):
-    var = json.dumps(data['Realtime Currency Exchange Rate'])
-    final = []
+    try:
+        var = json.dumps(data['Realtime Currency Exchange Rate'])
+        final = []
 
-    rl1 = re.search(r'("3.(.*?),)', var, )
-    rl2 = re.search(r'("4.(.*?),)', var, )
-    rl3 = re.search(r'("5.(.*?),)', var, )
-    rl4 = re.search(r'("6.(.*?),)', var, )
+        rl1 = re.search(r'("3.(.*?),)', var, )
+        rl2 = re.search(r'("4.(.*?),)', var, )
+        rl3 = re.search(r'("5.(.*?),)', var, )
+        rl4 = re.search(r'("6.(.*?),)', var, )
 
-    if rl1:
-        ob1 = rl1.group()
-        ss1 = re.search(r'(:(.*?)"(.*?)")', ob1, )
-        if ss1:
-            so1 = ss1.group()
-            fl1 = (re.sub(':|"', '', so1))
-            final.insert(1, fl1)
-        else:
-            print "ss1 has returned false"
-        if rl2:
-            ob2 = rl2.group()
-            ss2 = re.search(r'(:(.*?)"(.*?)")', ob2, )
-            if ss2:
-                so2 = ss2.group()
-                fl2 = (re.sub(':|"', '', so2))
-                final.insert(2, fl2)
+        if rl1:
+            ob1 = rl1.group()
+            ss1 = re.search(r'(:(.*?)"(.*?)")', ob1, )
+            if ss1:
+                so1 = ss1.group()
+                fl1 = (re.sub(':|"', '', so1))
+                final.insert(1, fl1)
             else:
-                print "ss2 has returned false"
-            if rl3:
-                ob3 = rl3.group()
-                ss3 = re.search(r'(:(.*?)"(.*?)")', ob3, )
-                if ss3:
-                    so3 = ss3.group()
-                    fl3 = (re.sub(':|"', '' , so3))
-                    final.insert(3, fl3)
-                else:
-                    print "ss3 has returned false"
-                if rl4:
-                    ob4 = rl4.group()
-                    ss4 = re.search(r'(:(.*?)"(.*?)")', ob4, )
-                    if ss4:
-                        so4 = ss4.group()
-                        fl4 = (re.sub(':|"', '', so4))
-                        final.insert(4, fl4)
+                print "ss1 has returned false"
+                if rl2:
+                    ob2 = rl2.group()
+                    ss2 = re.search(r'(:(.*?)"(.*?)")', ob2, )
+                    if ss2:
+                        so2 = ss2.group()
+                        fl2 = (re.sub(':|"', '', so2))
+                        final.insert(2, fl2)
                     else:
-                        print"ss4 has returned false"
+                        print "ss2 has returned false"
+                    if rl3:
+                        ob3 = rl3.group()
+                        ss3 = re.search(r'(:(.*?)"(.*?)")', ob3, )
+                    if ss3:
+                        so3 = ss3.group()
+                        fl3 = (re.sub(':|"', '' , so3))
+                        final.insert(3, fl3)
+                    else:
+                        print "ss3 has returned false"
+                    if rl4:
+                        ob4 = rl4.group()
+                        ss4 = re.search(r'(:(.*?)"(.*?)")', ob4, )
+                        if ss4:
+                            so4 = ss4.group()
+                            fl4 = (re.sub(':|"', '', so4))
+                            final.insert(4, fl4)
+                        else:
+                            print"ss4 has returned false"
+                    else:
+                        print "rl4 has returned false"
                 else:
-                    print "rl4 has returned false"
+                    print "rl3 has returned false"
             else:
-                print "rl3 has returned false"
+                print "rl2 returned false"
         else:
-            print "rl2 returned false"
-    else:
-        print "rl1 returned false"
+            print "rl1 returned false"
+    except IOError:
+        print "Error encountered!"
     return final
