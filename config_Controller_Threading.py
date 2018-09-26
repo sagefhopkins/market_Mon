421ee2a554627b629ec4f3db25bb2f29c809cb88
sagefhopkins <sagefhopkins@gmail.com>
2018-09-26 21:11:06 +0900

    Fixing Threading

--- config_Controller_Threading.py
+++ config_Controller_Threading.py
@@ -16,32 +16,30 @@ def config_Add_Cur(currency):
     print currency + " added to config.ini"
 
 def config_Load_Cur():
+    class Thread(threading.Thread, currency):
+        whileloop = 1
+        while whileloop == 1:
+            try:
+                data = jsc.json_Import_Cur(currency)
+                final = jsc.regex_Seperate(data)
+                dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
+                print Fore.GREEN + "-------------------------------------------------------------------------------------------"
+                print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
+                time.sleep(1)
+                whileloop = 0
+            except(IOError, TypeError, KeyError):
+                time.sleep(3)
+
     with open('config.ini') as f:
         conf = f.read()
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
-    config.readfp(io.BytesIO(conf))
-    for each_section in config.sections():
-        for (each_key, each_val) in config.items(each_section):
-            class Thread(threading.Thread):
-                def run(self):
-
-                    whileloop = 1
-                    if each_val == 'True':
-                        while whileloop == 1:
-                            try:
-                                data = jsc.json_Import_Cur(each_key)
-                                final = jsc.regex_Seperate(data)
-                                dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
-                                print Fore.GREEN + "-------------------------------------------------------------------------------------------"
-                                print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
-                                time.sleep(1)
-                                whileloop = 0
-                            except (IOError, TypeError, KeyError):
-                                time.sleep(3)
-                    else:
-                        print "config_Load_Cur failed"
-    for x in range(157):
-        thread = Thread(name = "Thread-{}".format(x + 1))
-        print "Starting Thread-{}".format(x + 1)
-        thread.start()
-        time.sleep(2)
+        config = ConfigParser.RawConfigParser(allow_no_value=True)
+        config.readfp(io.BytesIO(conf))
+        for each_section in config.sections():
+            for (each_key, each_val) in config.items(each_section):
+                if each_val == 'True':
+                    thread = Thread(name = "Thread-{}".format(each_key), each_key)
+                    print "Starting Thread-{}".format(each_key)
+                    thread.start()
+                    time.sleep(1)
+                else:
+                    print "Error starting Thread-{}".format(each_key)
