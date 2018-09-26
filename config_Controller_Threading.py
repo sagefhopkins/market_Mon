5783499a0b3cec246b972a96faa6571513fade4c
sagefhopkins <sagefhopkins@gmail.com>
2018-09-26 20:59:56 +0900

    Fixes for threading

--- config_Controller_Threading.py
+++ config_Controller_Threading.py
@@ -22,24 +22,24 @@ def config_Load_Cur():
     config.readfp(io.BytesIO(conf))
     for each_section in config.sections():
         for (each_key, each_val) in config.items(each_section):
-            print "Starting Thread! for " + each_key
-            if each_val == 'True':
-                class Thread(threading.Thread):
-                    def run(self):
-                        whileloop = 1
+            class Thread(threading.Thread):
+                def run(self):
+
+                    whileloop = 1
+                    if each_val == 'True':
                         while whileloop == 1:
                             try:
                                 data = jsc.json_Import_Cur(each_key)
                                 final = jsc.regex_Seperate(data)
                                 dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
-                                print Fore.GREEN + "-------------------------------------------------------------------------------------------"
-                                print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
-                                time.sleep(1)
-                                whileloop = 0
+                            print Fore.GREEN + "-------------------------------------------------------------------------------------------"
+                            print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
+                            time.sleep(1)
+                            whileloop = 0
                             except (IOError, TypeError, KeyError):
                                 time.sleep(3)
-            else:
-                print "config_Load_Cur failed"
+                    else:
+                        print "config_Load_Cur failed"
     for x in range(157):
         thread = Thread(name = "Thread-{}".format(x + 1))
         print "Starting Thread-{}".format(x + 1)
