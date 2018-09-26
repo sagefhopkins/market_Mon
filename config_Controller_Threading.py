2a56a3b2e0ae99982097ca5ef719337100c532a8
sagefhopkins <sagefhopkins@gmail.com>
2018-09-26 21:18:44 +0900

    Fixing Threading

--- config_Controller_Threading.py
+++ config_Controller_Threading.py
@@ -17,18 +17,21 @@ def config_Add_Cur(currency):
 
 def config_Load_Cur():
     class Thread(threading.Thread, currency):
-        whileloop = 1
-        while whileloop == 1:
-            try:
-                data = jsc.json_Import_Cur(currency)
-                final = jsc.regex_Seperate(data)
-                dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
-                print Fore.GREEN + "-------------------------------------------------------------------------------------------"
-                print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
-                time.sleep(1)
-                whileloop = 0
-            except(IOError, TypeError, KeyError):
-                time.sleep(3)
+        def init(currency):
+            print currency
+        def run(self):
+            whileloop = 1
+            while whileloop == 1:
+                try:
+                    data = jsc.json_Import_Cur(currency)
+                    final = jsc.regex_Seperate(data)
+                    dbc.database_Insert('cur_Mon', final[0], final[1], final[2], final[3])
+                    print Fore.GREEN + "-------------------------------------------------------------------------------------------"
+                    print Back.WHITE + Style.DIM + Fore.CYAN  + final[0] + Style.NORMAL + Fore.YELLOW + final[1]  + Fore.MAGENTA + Back.WHITE + final[2] + Fore.RED + Back.WHITE + final[3] + Fore.BLUE + Back.WHITE +" inserted into database" + Style.RESET_ALL
+                    time.sleep(1)
+                    whileloop = 0
+                except(IOError, TypeError, KeyError):
+                    time.sleep(3)
 
     with open('config.ini') as f:
         conf = f.read()
@@ -37,7 +40,7 @@ def config_Load_Cur():
         for each_section in config.sections():
             for (each_key, each_val) in config.items(each_section):
                 if each_val == 'True':
-                    thread = Thread(name = "Thread-{}".format(each_key), args=(each_key,))
+                    thread = Thread(name = "Thread-{}".format(each_key))
                     print "Starting Thread-{}".format(each_key)
                     thread.start()
                     time.sleep(1)
