87632a2c68deafb11edf806425009020c2416d10
sagefhopkins <sagefhopkins@gmail.com>
2018-09-26 20:23:52 +0900

    Added temporary pass over function when json_Controller throws error for ss4 has returned false, this issue has something to do with data being passed to json_Controller.regex_Seperate which isn't complete or was loaded in correctly. This issue should be resolved at a later date, however we will move past if for now

--- json_Controller.py
+++ json_Controller.py
@@ -15,67 +15,67 @@ def json_Import_Cur(currency):
 
     return data
 def regex_Seperate(data):
-    whileloop = 1
     try:
-        while whileloop == 1:
-            var = json.dumps(data['Realtime Currency Exchange Rate'])
-            final = []
-            rl1 = re.search(r'("3.(.*?),)', var, )
-            rl2 = re.search(r'("4.(.*?),)', var, )
-            rl3 = re.search(r'("5.(.*?),)', var, )
-            rl4 = re.search(r'("6.(.*?),)', var, )
-            if rl1:
-                ob1 = rl1.group()
-                ss1 = re.search(r'(:(.*?)"(.*?)")', ob1, )
-                if ss1:
-                    so1 = ss1.group()
-                    fl1 = (re.sub(':|"', '', so1))
-                    final.insert(1, fl1)
-                else:
-                    print "ss1 has returned false"
-                    whileloop == 0
-                if rl2:
-                    ob2 = rl2.group()
-                    ss2 = re.search(r'(:(.*?)"(.*?)")', ob2, )
-                    if ss2:
-                        so2 = ss2.group()
-                        fl2 = (re.sub(':|"', '', so2))
-                        final.insert(2, fl2)
-                    else:
-                        print "ss2 has returned false"
-                        whileloop == 0
-                    if rl3:
-                        ob3 = rl3.group()
-                        ss3 = re.search(r'(:(.*?)"(.*?)")', ob3, )
-                        if ss3:
-                            so3 = ss3.group()
-                            fl3 = (re.sub(':|"', '' , so3))
-                            final.insert(3, fl3)
-                        else:
-                            print "ss3 has returned false"
-                            whileloop == 0
-                        if rl4:
-                            ob4 = rl4.group()
-                            ss4 = re.search(r'(:(.*?)"(.*?)")', ob4, )
-                            if ss4:
-                                so4 = ss4.group()
-                                fl4 = (re.sub(':|"', '', so4))
-                                final.insert(4, fl4)
-                                return final
-                            else:
-                                print"ss4 has returned false"
-                                whileloop == 0
-                        else:
-                            print "rl4 has returned false"
-                            whileloop == 0
-                    else:
-                        print "rl3 has returned false"
-                        whileloop == 0
-                else:
-                    print "rl2 returned false"
-                    whileloop == 0
+        var = json.dumps(data['Realtime Currency Exchange Rate'])
+        final = []
+
+        rl1 = re.search(r'("3.(.*?),)', var, )
+        rl2 = re.search(r'("4.(.*?),)', var, )
+        rl3 = re.search(r'("5.(.*?),)', var, )
+        rl4 = re.search(r'("6.(.*?),)', var, )
+
+        if rl1:
+            ob1 = rl1.group()
+            ss1 = re.search(r'(:(.*?)"(.*?)")', ob1, )
+            if ss1:
+                so1 = ss1.group()
+                fl1 = (re.sub(':|"', '', so1))
+                final.insert(1, fl1)
             else:
-                print "rl1 returned false"
-                whileloop == 0
+                print "ss1 has returned false"
+                pass
+            if rl2:
+                ob2 = rl2.group()
+                ss2 = re.search(r'(:(.*?)"(.*?)")', ob2, )
+                if ss2:
+                    so2 = ss2.group()
+                    fl2 = (re.sub(':|"', '', so2))
+                    final.insert(2, fl2)
+                else:
+                    print "ss2 has returned false"
+                    pass
+                if rl3:
+                    ob3 = rl3.group()
+                    ss3 = re.search(r'(:(.*?)"(.*?)")', ob3, )
+                    if ss3:
+                        so3 = ss3.group()
+                        fl3 = (re.sub(':|"', '' , so3))
+                        final.insert(3, fl3)
+                    else:
+                        print "ss3 has returned false"
+                        pass
+                    if rl4:
+                        ob4 = rl4.group()
+                        ss4 = re.search(r'(:(.*?)"(.*?)")', ob4, )
+                        if ss4:
+                            so4 = ss4.group()
+                            fl4 = (re.sub(':|"', '', so4))
+                            final.insert(4, fl4)
+                            return final
+                        else:
+                            print"ss4 has returned false"
+                            pass
+                    else:
+                        print "rl4 has returned false"
+                        pass
+                else:
+                    print "rl3 has returned false"
+                    pass
+            else:
+                print "rl2 returned false"
+                pass
+        else:
+            print "rl1 returned false"
+            pass
     except(KeyError, TypeError):
         pass
