59ab28e91e3ec6a3150a1c342ca484f40e528ba2
sagefhopkins <sagefhopkins@gmail.com>
2018-09-26 22:49:52 +0900

    Test version of threading should work, need to test at rapid rate

--- config_Controller_Threading.py
+++ config_Controller_Threading.py
@@ -45,7 +45,8 @@ def config_Load_Cur():
                     thread = Thread(name = "Thread-{}".format(each_key), kwargs = {each_key: currency})
                     print "Starting Thread-{}".format(each_key)
                     thread.start()
-                    global var = 1
+                    global var
+                    var = 1
                     if var == 5:
                         print "Throttle waitng 62 seconds"
                         time.sleep(62)
