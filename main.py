1dc0768e2ed70ed963c3aeb74d6c8474f888e8d9
sagefhopkins <sagefhopkins@gmail.com>
2018-09-27 20:06:09 +0900

    Moving databasing into tables for each currency

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import importlib
 import database_Controller as database
 import json_Controller as jsc
-#import config_Controller_Threading as config
-import ConfigParser
-import io
-import time
+import config_Controller_Threading as config
 #import config_Controller as config
 
 #database.database_Read('gear', 'B')
@@ -15,6 +12,9 @@ import time
 #jsc.json_Interpret_Cur("ILS")
 #database.database_Table('cur_Mon')
 config.config_Load_Cur()
+
+database.database_Read('cur_Mon', 'ILS')
+
 # Table Desc
 #Curency,Currency Ticker, Exchange Rate , Open, High, Low, Close,
 #Monitor, Date
