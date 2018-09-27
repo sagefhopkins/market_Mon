3e0d8fa9cbaf597500e4e188c4745e286839534f
sagefhopkins <sagefhopkins@gmail.com>
2018-09-27 20:10:30 +0900

    Moving databasing into tables for each currency

--- database_Controller.py
+++ database_Controller.py
@@ -11,7 +11,7 @@ def database_Connect():
 def database_Insert(table, ticker, currency, exchange, date ):
     try:
         sql = database_Connect()
-        query = "INSERT INTO `" + table + "` VALUES('"null + + ticker + "', '" + currency + "', '" + exchange + "', '" + date + "')"
+        query = "INSERT INTO " + '`'+ table + '`' + " VALUES('INTERGER PRIMARY KEY, " + ticker + "', '" + currency + "', '" + exchange + "', '" + date + "')"
         sql.execute(query)
         sql.commit()
     except(TypeError, KeyError):
