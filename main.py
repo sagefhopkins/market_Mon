cda26d932f890271a9ff420ef2b89f5e8a745aa1
sagefhopkins <sagefhopkins@gmail.com>
2018-09-27 20:05:23 +0900

    Moving databasing into tables for each currency

--- main.py
+++ main.py
@@ -14,16 +14,7 @@ import time
 
 #jsc.json_Interpret_Cur("ILS")
 #database.database_Table('cur_Mon')
-#config.config_Load_Cur()
-
-with open('config.ini') as f:
-    conf = f.read()
-    config = ConfigParser.RawConfigParser(allow_no_value=True)
-    config.readfp(io.BytesIO(conf))
-    for each_section in config.sections():
-        for (each_key, each_val) in config.items(each_section):
-                database.database_Table(each_key)
-
+config.config_Load_Cur()
 # Table Desc
 #Curency,Currency Ticker, Exchange Rate , Open, High, Low, Close,
 #Monitor, Date
