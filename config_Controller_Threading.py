1034a40df1b6915dd36a97b8508b1cbd18250c7f
sagefhopkins <sagefhopkins@gmail.com>
2018-09-30 14:55:30 +0900

    Dev of key switch

--- config_Controller_Threading.py
+++ config_Controller_Threading.py
@@ -48,8 +48,8 @@ def config_Load_Cur():
             while api_Key_Array[0] == 'False':
                 api_Key_Num = api_Key_Num + 1
                 api_Key_Array = aku.new_Key_Pull(api_Key_Num)
-            except(TypeError):
-                pass
+        except(TypeError):
+            pass
         for each_section in config.sections():
             for (each_key, each_val) in config.items(each_section):
                 if var != 5:
